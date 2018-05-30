619d02692bc364e1388f3936ccdfb352cbea62de
Moshe Damari <mdamari@preempt.com>
2018-05-30 14:07:45 +0300

    removed key

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,10 +13,10 @@ public class Main {
       e.printStackTrace();
     }
     String setOAuthConsumerKey, setOAuthConsumerSecret, setOAuthAccessToken,setOAuthAccessTokenSecret;
-    setOAuthConsumerKey = "zmdHtwGBSGaxxC6KHifnvTmrb";
-    setOAuthConsumerSecret = "JI9Q7IFc7K5f2Lr7Ync1BEpmmtplmjfqtb6RR64fFV2eKuY3ws";
-    setOAuthAccessToken = "1280908806-e2PyK8ly1o0QqUB0pvwdyfVYd5x1EtutKtjQkIN";
-    setOAuthAccessTokenSecret = "jCWukDcXIWyKGT9MuUVFLB75lte6LHW3XKLFgFimf8655";
+    setOAuthConsumerKey = "";
+    setOAuthConsumerSecret = "";
+    setOAuthAccessToken = "";
+    setOAuthAccessTokenSecret = "";
 
     TwitterFeedListner mosheTwitter = new TwitterFeedListner(setOAuthConsumerKey ,setOAuthConsumerSecret, setOAuthAccessToken, setOAuthAccessTokenSecret , storage);
     mosheTwitter.Listen();
