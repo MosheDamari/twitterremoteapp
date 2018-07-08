9b73cd49d9ae67300f2dcc88f971210f2465958c
Moshe Damari <mdamari@preempt.com>
2018-07-08 14:24:44 +0300

    update storage

--- src/main/java/DB/DataStorage.java
+++ src/main/java/DB/DataStorage.java
@@ -31,17 +31,19 @@ public class DataStorage {
       String jdbcUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
       con = DriverManager.getConnection(jdbcUrl);
       Statement statement = con.createStatement();
+
       String twitTable = "CREATE TABLE twitTable " +
               "(id INTEGER not NULL , " +
               " name VARCHAR(256), " +
               " title VARCHAR(256), " +
               " link VARCHAR(256), " +
-              " content VARCHAR(256), " +
+              " content VARCHAR(10000), " +
               " image VARCHAR(256), " +
               " disc VARCHAR(256), " +
-              " date_created DATE, " +
+              " date_created TIMESTAMP, " +
               " PRIMARY KEY ( id ))";
       statement.executeUpdate(twitTable);
+
 
     }
     catch (ClassNotFoundException e) { System.out.println(e);}
@@ -59,7 +61,6 @@ public class DataStorage {
 
   }
   Connection conn;
-  int count = 1,newID = 1;
 
   public DataStorage() throws SQLException {
     this("twitterlinks.db");
@@ -69,70 +70,54 @@ public class DataStorage {
     String url = "jdbc:sqlite:" + database;
     conn = DriverManager.getConnection(url);
     Statement statement = conn.createStatement();
-
-    //The next line has the issue
-    String twitTable = "CREATE TABLE twitTable " +
-            "(id INTEGER not NULL , " +
-            " name VARCHAR(20), " +
-            " title VARCHAR(20), " +
-            " link VARCHAR(20), " +
-            " content VARCHAR(20), " +
-            " image VARCHAR(20), " +
-            " disc VARCHAR(20), " +
-            " date_created DATE, " +
-            " PRIMARY KEY ( id ))";
-    statement.executeUpdate(twitTable);
-
   }
 
   /**
-   * Add link to the database
+   * Add links to the database
    */
   public static void  AddLink(TwitterObj twitt,Connection con)
   {
     Connection conn = con;
-    // create a sql date object so we can use it in our INSERT statement
-    Calendar calendar = Calendar.getInstance();
-    java.sql.Date date_created = new java.sql.Date(calendar.getTime().getTime());
-    int count =0;
-    int newID = 0 ;
     // create the mysql insert preparedstatement
     try {
-
-      if (count - 1000 < 0)
-      {
-        String query = " insert into twitTable (id,name, title, link, content,image,disc,date_created)"
-                + " values (?, ?, ?, ?, ?, ?, ?, ?)";
-        PreparedStatement preparedStmt = conn.prepareStatement(query);
-        preparedStmt.setInt(1, count);
-        preparedStmt.setString(2, twitt.getName());
-        preparedStmt.setString(3, twitt.getTitle());
-        preparedStmt.setString(4, twitt.getLink());
-        preparedStmt.setString(5, twitt.getContent());
-        preparedStmt.setString(6, twitt.getImage());
-        preparedStmt.setString(7, twitt.getDisc());
-        preparedStmt.setDate(8, date_created);
-        preparedStmt.execute();
-        count++;
+        //if db is empty
+        String query ="SELECT COUNT(id) FROM twitTable";
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        int count = 0;
+        while(rs.next())
+        {
+          count = rs.getInt(1);
+        }
+        if(count == 0)
+        {
+          InsertToDB(count,twitt,con);
+        }
+        else
+        {
+          if (count < 1000) {
+            query = "SELECT MAX(id) FROM `twitterObj`.`twitTable`";
+            rs = st.executeQuery(query);
+            while (rs.next()) {
+              count = rs.getInt(1);
+              InsertToDB(count, twitt, con);
+            }
+          }
+          else
+            {
+              query = "SELECT MAX(id) FROM `twitterObj`.`twitTable`";
+              rs = st.executeQuery(query);
+              while (rs.next()) {
+                count = rs.getInt(1);
+                UpdateToDB(count, twitt, con);
+              }
+            }
+        }
       }
-      else
-      {
-        newID = count % 1000 ;
-        System.out.println(newID);
-        PreparedStatement preparedStmt = conn.prepareStatement("update twitTable set name = ?, title = ?, link = ?, content = ?, image = ?, disc = ?, date_crated = ?  where id = ?");
-        preparedStmt.setString(1, twitt.getName());
-        preparedStmt.setString(3, twitt.getLink());
-        preparedStmt.setString(2, twitt.getTitle());
-        preparedStmt.setString(4, twitt.getContent());
-        preparedStmt.setString(5, twitt.getImage());
-        preparedStmt.setString(6, twitt.getDisc());
-        preparedStmt.setDate(7, date_created);
-        preparedStmt.executeUpdate();
-        count++;
-      }
-    } catch (SQLException e) {
+     catch (SQLException e)
+     {
       e.printStackTrace();
-    }
+     }
 
   }
   public TwitterObj[] ReadMySql(int amount)
@@ -163,5 +148,41 @@ public class DataStorage {
 
 
     return objs;
+  }
+  public static void InsertToDB(int count,TwitterObj twitt,Connection con) throws SQLException
+  {
+    Connection conn = con;
+    Calendar calendar = Calendar.getInstance();
+    java.sql.Timestamp date_created = new java.sql.Timestamp(calendar.getTime().getTime());
+    count++;
+    String query = " insert into twitTable (id,name, title, link, content,image,disc,date_created)"
+            + " values (?, ?, ?, ?, ?, ?, ?, ?)";
+    PreparedStatement preparedStmt = conn.prepareStatement(query);
+    preparedStmt = conn.prepareStatement(query);
+    preparedStmt.setInt(1, count);
+    preparedStmt.setString(2, twitt.getName());
+    preparedStmt.setString(3, twitt.getTitle());
+    preparedStmt.setString(4, twitt.getLink());
+    preparedStmt.setString(5, twitt.getContent());
+    preparedStmt.setString(6, twitt.getImage());
+    preparedStmt.setString(7, twitt.getDisc());
+    preparedStmt.setTimestamp(8, date_created);
+    preparedStmt.execute();
+  }
+  public static void UpdateToDB(int count,TwitterObj twitt,Connection con) throws SQLException
+  {
+    Connection conn = con;
+    Calendar calendar = Calendar.getInstance();
+    java.sql.Timestamp date_created = new java.sql.Timestamp(calendar.getTime().getTime());
+    count++;
+    PreparedStatement preparedStmt = conn.prepareStatement("update twitTable set name = ?, title = ?, link = ?, content = ?, image = ?, disc = ?, date_crated = ?  where id = ?");
+    preparedStmt.setString(1, twitt.getName());
+    preparedStmt.setString(3, twitt.getLink());
+    preparedStmt.setString(2, twitt.getTitle());
+    preparedStmt.setString(4, twitt.getContent());
+    preparedStmt.setString(5, twitt.getImage());
+    preparedStmt.setString(6, twitt.getDisc());
+    preparedStmt.setTimestamp(7, date_created);
+    preparedStmt.executeUpdate();
   }
 }
